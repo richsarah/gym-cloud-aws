7226d39978f3d1162fe192c0f99c2d2dea88821a
sarah <sarah.chenglu@gmail.com>
2021-11-24 12:07:15 -0500

    first commit
    first commit

---
+++ demo-aws/src/main/java/com/example/demoaws/DemoAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsApplication.class, args);
+	}
+
+}
---
+++ demo-aws/src/test/java/com/example/demoaws/DemoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
